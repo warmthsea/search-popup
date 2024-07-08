614b81be2868a4e17ee249bcbebb9c71f5665fe8
warmthsea <2586244885@qq.com>
2024-07-08 15:54:38 +0800

    chore: add vueuse and auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,17 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [
+    vue(),
+    dts(),
+    AutoImport({
+      imports: ['vue', '@vueuse/core'],
+      dts: true,
+    }),
+  ],
   build: {
     rollupOptions: {
       external: ['vue'],
